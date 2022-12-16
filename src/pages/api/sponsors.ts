8efca3c2c8c6f86df0217758e19387ada26e8e8a
luxumbra <me@luxumbra.dev>
2022-12-16 17:22:55 +0000

    Add sponsors list / endpoint

---
+++ src/pages/api/sponsors.ts
@@ -0,0 +1,31 @@
+import { APIRoute } from "astro"
+
+export const get: APIRoute = async function get ({ params, request }) {
+  return {
+    body: JSON.stringify({
+      message: 'Welcome to our sponsors & partners!',
+      sponsorList: [
+        {
+          "name": "MetaCartel",
+          "logo": "metacartel.png",
+          "url": "https://metacartel.xyz"
+        },
+        {
+          "name": "MetaFam",
+          "logo": "metagame.png",
+          "url": "https://metagame.wtf"
+        },
+        {
+          name: 'Sponsor 1',
+          logo: 'metacartel.png',
+          link: 'https://google.com',
+        },
+        {
+          name: 'Sponsor 2',
+          logo: 'metagame.png',
+          link: 'https://google.com',
+        },
+      ],
+    }),
+  }
+}
