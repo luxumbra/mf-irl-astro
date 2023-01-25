182a3e3a84a4f1ef123512712c928178b3015fd4
luxumbra <me@luxumbra.dev>
2023-01-25 14:31:00 +0000

    quite a few tweaks after feedback & design changes from peth

--- src/components/widgets/ThreeD.tsx
+++ src/components/widgets/ThreeD.tsx
@@ -10,7 +10,7 @@ const ThreeD = () => {
   return (
     <div className="fixed top-0 left-0 w-screen h-full z-[2] pointer-events-none">
       <Canvas
-        className='fixed top-0 left-0 w-screen h-full pointer-events-none bg-slate-900'
+        className='fixed top-0 left-0 w-screen h-full pointer-events-none bg-secondary-dark'
       >
         <Experience />
       </Canvas>
