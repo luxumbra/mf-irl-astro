c8f24d5a724543d30eb06b9136560cd8858cc44c
luxumbra <me@luxumbra.dev>
2023-06-27 17:35:49 +0100

    Fix bad Notion url

--- src/config.mjs
+++ src/config.mjs
@@ -24,6 +24,8 @@ export const earlyBirdEndsFormatted = DateTime.fromISO(earlyBirdEnds).toLocaleSt
 export const STRIPE_DISCOUNT_KEY_STANDARD = isProd ? '9AQaH95IAalcbf24gg' : 'test_8wM6qfbXfgm1gi4bII';
 export const STRIPE_DISCOUNT_KEY_PATRON = isProd ? '5kA8z14Ew64W5UIbIJ' : 'test_cN2bKzd1jd9P7Ly4gi';
 
+export const FAQ_URL = 'https://meta-game.notion.site/MetaFest-FAQ-ff18e665f7964264854f3d7191a04959?pvs=4';
+
 export const SITE = {
 	name: 'MetaFest: Croatia',
 	origin: HOSTNAME ?? 'https://metafest.wtf',
