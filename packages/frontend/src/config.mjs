e8044da4df8a6b3f5fda9c585c023f9fcb3ece45
luxumbra <me@luxumbra.dev>
2023-08-07 23:01:45 +0100

    latest changes

--- packages/frontend/src/config.mjs
+++ packages/frontend/src/config.mjs
@@ -21,8 +21,9 @@ export const earlyBirdEnds = '2023-05-01';
 export const eventStartDate = '2023-08-11';
 export const earlyBirdEndsFormatted = DateTime.fromISO(earlyBirdEnds).toLocaleString(DateTime.DATE_SHORT);
 
-export const STRIPE_DISCOUNT_KEY_STANDARD = isProd ? '9AQaH95IAalcbf24gg' : 'test_8wM6qfbXfgm1gi4bII';
-export const STRIPE_DISCOUNT_KEY_PATRON = isProd ? '5kA8z14Ew64W5UIbIJ' : 'test_cN2bKzd1jd9P7Ly4gi';
+export const STRIPE_DISCOUNT_KEY_STANDARD = '9AQaH95IAalcbf24gg';
+export const STRIPE_DISCOUNT_KEY_PATRON = '5kA8z14Ew64W5UIbIJ';
+export const FACEBOOK_PIXEL_ID = import.meta.env.PUBLIC_FACEBOOK_PIXEL_ID;
 
 export const FAQ_URL = 'https://meta-game.notion.site/MetaFest-FAQ-ff18e665f7964264854f3d7191a04959?pvs=4';
 
