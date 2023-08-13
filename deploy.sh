76b768fd3fb34fa11eebb2e6d395954b8098da43
luxumbra <me@luxumbra.dev>
2023-08-13 18:46:28 +0100

    more tweak for the deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,18 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 cd $SCRIPT_DIR
 
 # Pull the latest code
-git pull origin develop
+git fetch && git pull origin develop
+
+# Check if pnpm is installed
+if ! command -v pnpm &> /dev/null; then
+    echo "pnpm not found! Installing..."
+    npm install -g pnpm
+else
+    echo "pnpm is already installed."
+fi
+
+# Install dependencies
+pnpm install
 
 # Run the build command
 pnpm frontend:build
